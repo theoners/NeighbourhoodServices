43e26ffebbb7e15f52db951e318bbf74db3251da
theoners <theonebgrs@gmail.com>
2020-03-05 22:28:01 +0100

    implement Announcement Cotroler

---
+++ Web/NeighbourhoodServices.Web/Controllers/AnnouncementController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NeighbourhoodServices.Web.Controllers
+{
+    public class AnnouncementController : BaseController
+    {
+        public IActionResult Announcement()
+        {
+            return this.View();
+        }
+    }
+}
--- Web/NeighbourhoodServices.Web/Startup.cs
+++ Web/NeighbourhoodServices.Web/Startup.cs
@@ -58,6 +58,10 @@
             // Application services
             services.AddTransient<IEmailSender, NullMessageSender>();
             services.AddTransient<ISettingsService, SettingsService>();
+
+            // Runtime Refresh
+            services.AddRazorPages()
+                .AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
