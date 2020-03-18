c77631dfec43ea1ba92f0ee34ef3f069b08e1b9f
theoners <theonebgrs@gmail.com>
2020-03-19 00:17:07 +0100

    update Announcements func

--- Web/NeighbourhoodServices.Web.ViewModels/Announcements/AnnouncementViewModel.cs
+++ Web/NeighbourhoodServices.Web.ViewModels/Announcements/AnnouncementViewModel.cs
@@ -1,21 +1,19 @@
 ﻿namespace NeighbourhoodServices.Web.ViewModels.Announcement
 {
-
     using NeighbourhoodServices.Data.Models;
     using NeighbourhoodServices.Services.Mapping;
 
     public class AnnouncementViewModel : IMapFrom<Announcement>
     {
+        public string Title { get; set; }
         public string Description { get; set; }
 
         public ServiceType ServiceType { get; set; }
 
         public string Place { get; set; }
 
-       
+        public string CreatedOn { get; set; }
         public string CategoriesName { get; set; }
-
-        
 
         public virtual string User { get; set; }
     }
