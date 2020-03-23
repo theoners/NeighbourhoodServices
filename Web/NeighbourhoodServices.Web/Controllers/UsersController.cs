a7c9d0084af7a224a00643329f1870866c676e45
theoners <theonebgrs@gmail.com>
2020-03-23 23:11:19 +0100

    update login and Register

--- Web/NeighbourhoodServices.Web/Controllers/UsersController.cs
+++ Web/NeighbourhoodServices.Web/Controllers/UsersController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using NeighbourhoodServices.Data.Models;
 using NeighbourhoodServices.Web.Areas.Identity.Pages.Account;
 
@@ -13,32 +16,56 @@ namespace NeighbourhoodServices.Web.Controllers
     {
         private readonly UserManager<ApplicationUser> userManager;
         private readonly SignInManager<ApplicationUser> signInManager;
+        private readonly ILogger<LoginModel> logger;
 
 
-        public UsersController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        public UsersController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, ILogger<LoginModel> logger)
         {
             this.userManager = userManager;
             this.signInManager = signInManager;
+            this.logger = logger;
         }
 
-        public async Task<IActionResult> Login(string userName, string password, string email)
+        public async Task<IActionResult> Login(string userName, string password)
         {
 
-            var result = await this.signInManager.PasswordSignInAsync(userName, password, true, true);
-               
+            if (this.ModelState.IsValid)
+            {
+                // This doesn't count login failures towards account lockout
+                // To enable password failures to trigger account lockout, set lockoutOnFailure: true
+                var user = await this.userManager.Users
+                    .FirstOrDefaultAsync(u => u.UserName == userName);
+                if (user != null)
+                {
+                    var result = await this.signInManager.PasswordSignInAsync(user.UserName, password, true, lockoutOnFailure: false);
+                    if (result.Succeeded)
+                    {
+                        this.logger.LogInformation("User logged in.");
+                        return this.LocalRedirect("/");
+                    }
+
+                    //else
+                    //{
+                    //    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                    //    return this.PartialView("_LoginRegisterPopUp", "false");
+                    //}
+                }
+            }
+
+
 
             return this.Redirect("/");
         }
         public async Task<IActionResult> Register(string userName, string password, string email)
         {
 
-            var result= await this.userManager.CreateAsync(
+            var result = await this.userManager.CreateAsync(
                 new ApplicationUser()
-            {
-                UserName = userName,
-                Email = email,
+                {
+                    UserName = userName,
+                    Email = email,
 
-            }, password);
+                }, password);
 
             return this.Redirect("/");
         }
--- Web/NeighbourhoodServices.Web/wwwroot/js/login-register.js
+++ Web/NeighbourhoodServices.Web/wwwroot/js/login-register.js
@@ -1,71 +1,19 @@
-/*
- *
- * login-register modal
- * Autor: Creative Tim
- * Web-autor: creative.tim
- * Web script: http://creative-tim.com
- * 
- */
-function showRegisterForm(){
-    $('.loginBox').fadeOut('fast',function(){
-        $('.registerBox').fadeIn('fast');
-        $('.login-footer').fadeOut('fast',function(){
-            $('.register-footer').fadeIn('fast');
-        });
-        $('.modal-title').html('Register with');
-    }); 
-    $('.error').removeClass('alert alert-danger').html('');
-       
-}
-function showLoginForm(){
-    $('#loginModal .registerBox').fadeOut('fast',function(){
-        $('.loginBox').fadeIn('fast');
-        $('.register-footer').fadeOut('fast',function(){
-            $('.login-footer').fadeIn('fast');    
-        });
-        
-        $('.modal-title').html('Login with');
-    });       
-     $('.error').removeClass('alert alert-danger').html(''); 
+function toggleResetPswd(e) {
+    e.preventDefault();
+    $('#logreg-forms .form-signin').toggle() // display:block or none
+    $('#logreg-forms .form-reset').toggle() // display:block or none
 }
 
-function openLoginModal(){
-    showLoginForm();
-    setTimeout(function(){
-        $('#loginModal').modal('show');    
-    }, 230);
-    
-}
-function openRegisterModal(){
-    showRegisterForm();
-    setTimeout(function(){
-        $('#loginModal').modal('show');    
-    }, 230);
-    
+function toggleSignUp(e) {
+    e.preventDefault();
+    $('#logreg-forms .form-signin').toggle(); // display:block or none
+    $('#logreg-forms .form-signup').toggle(); // display:block or none
 }
 
-function loginAjax(){
-    /*  Remove this comments when moving to server
-    $.post( "/login", function( data ) {
-            if(data == 1){
-                window.location.replace("/home");            
-            } else {
-                 shakeModal(); 
-            }
-        });
-    */
-
-/*   Simulate error message from the server   */
-     shakeModal();
-}
-
-function shakeModal(){
-    $('#loginModal .modal-dialog').addClass('shake');
-             $('.error').addClass('alert alert-danger').html("Invalid email/password combination");
-             $('input[type="password"]').val('');
-             setTimeout( function(){ 
-                $('#loginModal .modal-dialog').removeClass('shake'); 
-    }, 1000 ); 
-}
-
-   
+$(() => {
+    // Login Register Form
+    $('#logreg-forms #forgot_pswd').click(toggleResetPswd);
+    $('#logreg-forms #cancel_reset').click(toggleResetPswd);
+    $('#logreg-forms #btn-signup').click(toggleSignUp);
+    $('#logreg-forms #cancel_signup').click(toggleSignUp);
+})
